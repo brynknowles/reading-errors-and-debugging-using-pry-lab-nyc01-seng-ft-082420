df208610e4065c7e9a2e48ddc067bac4ed676f3f
Bryn Knowles <bryn.knowles@gmail.com>
2020-07-05 17:01:41 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,17 @@
 # don't forget to add: require 'pry'
+require 'pry'
+
+# def snake_it_up(string)
+#   if string[0] == "s"
+#     10 * "s" + string
+#   else
+#     string
+#   end
+# end
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if x = 10
+    string[0] * x + string
   else
     string
   end
